347484b512ffda7d047794ce73d9701a9da5ac07
J-keven <jhonasnunes425@gmail.com>
2020-04-04 19:52:26 -0300

    add password athentication

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -8,42 +8,48 @@ import api from '../../services/api'
 
 export default function Login() {
 
-	const [ id, setId ] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
 	const history = useHistory()
-	async function handleLoign(e){
+	async function handleLoign(e) {
 		e.preventDefault()
 		try {
 
-			const response = await api.post('/login', { id })
+			const response = await api.post('/login', { email, password })
 			localStorage.setItem('ongToken', response.data.token)
 			localStorage.setItem('ongName', response.data.name)
 			history.push('/profile')
 
-		} catch(error){
+		} catch (error) {
 			alert("Houve um erro ao fazer login")
 		}
-	
+
 	}
 	return (
 		<div className="login-container">
 			<section className="form">
-				<img src={logoImg} alt="LogoImg"/>
+				<img src={logoImg} alt="LogoImg" />
 				<form onSubmit={handleLoign}>
 					<h1>Faça seu logon</h1>
-					<input placeholder="Sua ID"
-						value={id}
-						onChange={e => setId(e.target.value)}
+					<input placeholder="Username"
+						value={email}
+						onChange={e => setEmail(e.target.value)}
+						style={{ marginBottom: 10 }}
+					/>
+					<input placeholder="Password"
+						value={password}
+						type='password'
+						onChange={e => setPassword(e.target.value)}
 					/>
 					<button className="button" type="submit">Entrer</button>
-					{/* <br /> */}
 					<Link className='back-link' to="/register">
-						<FiLogIn size={16} color="#E02041"/>
+						<FiLogIn size={16} color="#E02041" />
 						Não tenho cadastro
 					</Link>
 				</form>
 			</section>
 
-			<img src={heroImg} alt="HeroImg"/>
+			<img src={heroImg} alt="HeroImg" />
 		</div>
 	);
 }
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -8,24 +8,26 @@ import logoImg from '../../assets/logo.svg'
 import './style.css'
 
 
-function Register(){
-	
-	const [name, setName ] = useState('')
-	const [email, setEmail ] = useState('')
-	const [whatsapp, setWhatsapp ] = useState('')
-	const [city, setCity ] = useState('')
-	const [uf, setUf ] = useState('')
-	
+function Register() {
+
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [whatsapp, setWhatsapp] = useState('')
+	const [city, setCity] = useState('')
+	const [uf, setUf] = useState('')
+	const [password, setPassword] = useState('')
+
 	const history = useHistory()
-	async function hanldeRegister(e){
+	async function hanldeRegister(e) {
 		e.preventDefault();
-		
-		const data = { 
-			name, 
-			email, 
-			whatsapp, 
-			city, 
-			uf
+
+		const data = {
+			name,
+			email,
+			whatsapp,
+			city,
+			uf,
+			password
 		}
 		try {
 			const response = await api.post('/ongs', data)
@@ -41,46 +43,53 @@ function Register(){
 		<div className="register-container">
 			<div className="content">
 				<section className="form">
-					<img src={logoImg} alt="Logo"/>
+					<img src={logoImg} alt="Logo" />
 					<h1>Cadastro</h1>
 					<p>Faça seu cadastro. entre na plataforma e ajude pessoas a encontrar os casos da sua ONG.</p>
 
 					<Link className='back-link' to="/">
-						<FiArrowLeft size={16} color="#E02041"/>
+						<FiArrowLeft size={16} color="#E02041" />
 						Voltar para o login
 					</Link>
 				</section>
 
 				<form onSubmit={hanldeRegister}>
-					<input 
+					<input
 						placeholder="Nome da ONG"
 						value={name}
 						onChange={e => setName(e.target.value)}
 					/>
-					<input 
-						type="email" 
+					<input
+						type="email"
 						placeholder="E-mail"
 						value={email}
 						onChange={e => setEmail(e.target.value)}
 					/>
-					<input 
+					<input
 						placeholder="WhatsApp"
 						value={whatsapp}
 						onChange={e => setWhatsapp(e.target.value)}
 					/>
 					<div className="input-group">
-						<input  
+						<input
 							placeholder="Cidade"
 							value={city}
 							onChange={e => setCity(e.target.value)}
 						/>
-						<input  
-							placeholder="UF" 
+						<input
+							placeholder="UF"
 							style={{ width: 80 }}
 							value={uf}
 							onChange={e => setUf(e.target.value)}
 						/>
 					</div>
+					<input
+						type='password'
+						placeholder="Senha"
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
+
 					<button className="button" type="submit">Castrar</button>
 				</form>
 			</div>
